19c3fd9be51ebece4ac220cd502dbe453eebdb93
Ain Tohvri <ain@flashbit.net>
2013-05-20 23:55:31 +0200

    Upgrade Grunt, simplify build
    - Grunt upgrade to 0.4.1
    - Inclusion of all dependencies
    - Single package descriptor
    - Only minified version in distribution, no concatenation

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Enable Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: {
+        src: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+        options: {
+          curly: true,
+          eqeqeq: true,
+          immed: true,
+          latedef: true,
+          newcap: true,
+          noarg: true,
+          sub: true,
+          undef: true,
+          boss: true,
+          eqnull: true,
+          browser: true,
+          globals: {
+            jQuery: true
+          }
+        }
+      }
+    },
+    pkg: grunt.file.readJSON('package.json'),
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+          '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+          '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+          ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> \n*/\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<banner:meta.banner>', 'src/<%= pkg.name %>.js']
+        }
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+  // Travis CI task
+  grunt.registerTask('travis', ['jshint']);
+};
