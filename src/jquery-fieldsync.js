4a32f499f843aae7c1e11bb4b2cf7bcf92c9d90d
Ain Tohvri <ain@flashbit.net>
2013-05-21 01:36:12 +0200

    Fix consecutive syncing failure on jQuery Mobile

--- src/jquery-fieldsync.js
+++ src/jquery-fieldsync.js
@@ -8,9 +8,6 @@
 
 (function($) {
   $.fn.fieldsync = function(src) {
-    if (this.val().length > 0) {
-      return;
-    }
     var eventId = 'keyup.fieldsync.' + this.attr('name').replace('[', '_').replace(']', '_');
     var field = this;
     this.val($(src).val());
